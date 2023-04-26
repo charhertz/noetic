13bc0f949db4ee9fb193a564753fef524428b86b
Charlie <hi@charhertz.com>
2023-04-26 22:26:22 +0800

    feat: site config

---
+++ src/site.config.mjs
@@ -0,0 +1,33 @@
+const siteConfig = {
+	siteName: "Charlie's Blog",
+	siteLanguage: "zh-Hans",
+	description: "Charlie's blog, about programming",
+	shortDescription: "undefined",
+	avatarPath: "/avatar.jpg",
+	faviconPath: "/favicon.svg",
+	links: [
+		{
+			type: "GitHub",
+			url: "https://github.com/devscharlie",
+		},
+		{
+			type: "Twitter",
+			url: "https://twitter.com/DevsCharlie",
+		},
+		{
+			type: "Email",
+			url: "mailto:hi@devscharlie.com",
+		},
+	],
+	codeTheme: {
+		light: "github-light",
+		dark: "github-dark",
+	},
+	siteUrl: "https://tailno.vercel.app",
+	authorName: "Charlie",
+	authorLink: "https://devscharlie.com",
+	authorEmail: "hi@devscharlie.com",
+	timeZone: "Asia/Shanghai",
+}
+
+export default siteConfig
